48caa06621cffaa9eb5b6b3efd680ec49b9d9ece
boda <bodayasser479@gmail.com>
2024-07-29 20:47:04 +0300

    init app

---
+++ app.js
@@ -0,0 +1,13 @@
+let a = document.getElementById("loginBtn");
+let b = document.getElementById("registerBtn");
+let x = document.getElementById("login");
+let y = document.getElementById("register");
+
+function login() {
+  x.style.left = "4px";
+  y.style.right = "-520px";
+}
+function register() {
+  x.style.left = "-520px";
+  y.style.right = "4px";
+}
